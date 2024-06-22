cc16c31a44c5e56bb0878fb1eba03abee99957bb
andrea <andrea.zollini@proton.me>
2024-06-22 16:30:55 +0200

    header fix

--- resources/js/Components/Partials/Header.vue
+++ resources/js/Components/Partials/Header.vue
@@ -4,7 +4,7 @@
             class="flex items-center justify-between p-6 mx-auto max-w-7xl gap-x-6 lg:px-8"
             aria-label="Global"
         >
-            <div class="flex lg:flex-1">
+            <div class="flex">
                 <a :href="route('home')" class="-m-1.5 p-1.5">
                     <span class="sr-only">Your Company</span>
                     <ApplicationLogo />
@@ -19,18 +19,66 @@
                     >{{ item.name }}</a
                 >
             </div>
-            <div class="flex items-center justify-end flex-1 gap-x-6">
-                <a
-                    :href="route('login')"
-                    class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
-                    >Log in</a
-                >
-                <Link
-                    :href="route('register')"
-                    class="px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >Sign up
-                </Link>
-            </div>
+            <template v-if="!$page.props.auth.user">
+                <div class="flex items-center justify-end gap-x-6">
+                    <a
+                        :href="route('login')"
+                        class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
+                        >Log in</a
+                    >
+                    <Link
+                        :href="route('register')"
+                        class="px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >Sign up
+                    </Link>
+                </div>
+            </template>
+            <template v-else>
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <!-- Settings Dropdown -->
+                    <div class="relative ms-3">
+                        <Dropdown align="right" width="48">
+                            <template #trigger>
+                                <span class="inline-flex rounded-md">
+                                    <button
+                                        type="button"
+                                        class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out border border-transparent rounded-md hover:text-gray-700 focus:outline-none"
+                                    >
+                                        {{ $page.props.auth.user.name }}
+
+                                        <svg
+                                            class="ms-2 -me-0.5 h-4 w-4"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd"
+                                            />
+                                        </svg>
+                                    </button>
+                                </span>
+                            </template>
+
+                            <template #content>
+                                <DropdownLink :href="route('profile.edit')">
+                                    Profile
+                                </DropdownLink>
+                                <DropdownLink
+                                    :href="route('logout')"
+                                    method="post"
+                                    as="button"
+                                >
+                                    Log Out
+                                </DropdownLink>
+                            </template>
+                        </Dropdown>
+                    </div>
+                </div>
+            </template>
+
             <div class="flex lg:hidden">
                 <button
                     type="button"
@@ -96,6 +144,8 @@
 </template>
 
 <script setup>
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
 import { ref } from "vue";
 import { Dialog, DialogPanel } from "@headlessui/vue";
 import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
@@ -103,7 +153,7 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import { Link } from "@inertiajs/vue3";
 
 const navigation = [
-    { name: "Products", href: "#" },
+    { name: "Products", href: "/products" },
     // { name: "Features", href: "#" },
     // { name: "Marketplace", href: "#" },
     { name: "Company", href: "#" },
